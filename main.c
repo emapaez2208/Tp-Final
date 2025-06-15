b88b5da3e9ec6f429cde85f5f42991532aa0d78c
DESIMONE-DEV <desimone.oscar.ale@gmail.com>
2025-06-15 05:08:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,30 @@
 #include <conio.h>
 #include <string.h>
 
+typedef struct
+{
+    int ID;                      /// ID:para numero de imagen;
+    char nombre[30];             /// Nombre de la Imagen;
+    char dibujo[40][100];        /// Con 100 espacios llega a mitad de pantalla;
+    int fila;                    /// Variable para imprimir la cant de filas justas;
+}stimagen;
+
+///Prototipado
+
+void cargaImagenes (char archivo[],stimagen Imagen[]); /// Archivo de imagen
+void VerImagen (stimagen imagen);  /// Por parametro la imagen a ver;
 
 
 int main()
 {
-pepito
-juancito
     
-
-
     return 0;
+}
+
+
+void VerImagen (stimagen imagen) 
+{
+    for (int i = 0; i < imagen.fila; i++) {
+        printf("%s\n", imagen.dibujo[i]);
+    }
 }
